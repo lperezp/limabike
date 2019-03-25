2cc94314ba78e3babe593021e1455354f7f08f85
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-03-24 23:30:08 -0500

    feat(CicloviaInterface): The interface was added.

---
+++ src/app/models/ciclovia-interface.ts
@@ -0,0 +1,9 @@
+export interface CicloviaInterface {
+  TipoSegregacion: string;
+  caracteristicas: string;
+  desde: string;
+  distancia: string;
+  hasta: string;
+  nombre: string;
+  tipoRuta: string;
+}
